82b49f4bf0b4d8d7a276a017446ed8934aee71f8
sharifmrahat <sharifmrahat@gmail.com>
2024-02-12 11:49:41 +0600

    redis string types is added

---
+++ string.js
@@ -0,0 +1,10 @@
+const client = require('./client')
+
+async function init(){
+    // await client.set("user:104", "Ryan Dahl")
+    // await client.expire("user:104", 10)
+    const result = await client.get("user:104")
+    console.log({result});
+}
+
+init()
